29ba0f6591f8e33561cd653e9fb29844839ade5c
Luca Minuti <luca.minuti@gmail.com>
2025-09-06 09:41:20 +0200

    Fix some unit names

--- Demo/MCPTest.dpr
+++ Demo/MCPTest.dpr
@@ -5,11 +5,11 @@ uses
   MCP.Form.Main in 'MCP.Form.Main.pas' {frmMain},
   MCP.Tools in '..\Source\MCP.Tools.pas',
   MCP.Attributes in '..\Source\MCP.Attributes.pas',
-  MCP.Invoker in '..\Source\MCP.Invoker.pas',
   JSON.RPC in '..\Source\JSON.RPC.pas',
   MCP.Types in '..\Source\MCP.Types.pas',
   MCP.Resources in '..\Source\MCP.Resources.pas',
-  MCP.Prompts in '..\Source\MCP.Prompts.pas';
+  MCP.Prompts in '..\Source\MCP.Prompts.pas',
+  JSON.RPC.Invoker in '..\Source\JSON.RPC.Invoker.pas';
 
 {$R *.res}
 

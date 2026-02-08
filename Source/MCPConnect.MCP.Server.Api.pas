9b9b9b9b95cbfc830230c034654d5f30a23b2057
paolo-rossi <paolo@paolorossi.net>
2026-02-08 10:45:28 +0100

    Initial support for the resources and logging APIs

--- Source/MCPConnect.MCP.Server.Api.pas
+++ Source/MCPConnect.MCP.Server.Api.pas
@@ -30,31 +30,46 @@ type
   [JRPC('tools')]
   TMCPToolsApi = class
   public
-    [Context]
-    MCPConfig: TMCPConfig;
-
-    [Context]
-    Context: TJRPCContext;
+    [Context] RPCContext: TJRPCContext;
+    [Context] MCPConfig: TMCPConfig;
 
     [JRPC('list')]
-    function List: TListToolsResult;
+    function ToolsList: TListToolsResult;
 
     [JRPC('call')]
-    function Call(
-        [JRPCParam('name')] const AName: string;
-        [JRPC('arguments')] AArguments: TJSONObject;
-        [JRPC('_meta')] Meta: TJSONObject): TCallToolResult;
+    function CallTool(
+      [JRPCParam('name')] const AName: string;
+      [JRPC('arguments')] AArguments: TJSONObject;
+      [JRPC('_meta')] Meta: TJSONObject): TCallToolResult;
   end;
+
+  [JRPC('resources')]
+  TMCPResourcesApi = class
+  public
+    [Context] RPCContext: TJRPCContext;
+    [Context] MCPConfig: TMCPConfig;
+
+    [JRPC('list')]
+    function ResourcesList: TListResourcesResult;
+
+    [JRPC('templates/list')]
+    function TemplatesList: TListResourceTemplatesResult;
+
+    [JRPC('read')]
+    function ReadResource(
+      [JRPCParam('name')] const AName: string;
+      [JRPC('arguments')] AArguments: TJSONObject;
+      [JRPC('_meta')] Meta: TJSONObject): TReadResourceResult;
+  end;
+
 
   [JRPC('notifications')]
   TMCPNotificationsApi = class
   public
-    [JRPC('initialized')]
-    [JRPCNotification]
+    [JRPC('initialized'), JRPCNotification]
     procedure Initialized;
 
-    [JRPC('cancelled')]
-    [JRPCNotification]
+    [JRPC('cancelled'), JRPCNotification]
     procedure Cancelled([JRPCParams] ACancelledParams: TCancelledNotificationParams);
 
   end;
@@ -62,12 +77,22 @@ type
   [JRPC('initialize')]
   TMCPInitializeApi = class
   public
-    [Context]
-    MCPConfig: TMCPConfig;
+    [Context] MCPConfig: TMCPConfig;
 
     [JRPC('')]
     function Initialize([JRPCParams] AInitializeParams: TInitializeParams): TInitializeResult;
   end;
+
+  [JRPC('logging')]
+  TMCPLoggingApi = class
+  public
+    [Context] MCPConfig: TMCPConfig;
+
+    [JRPC('setLevel')]
+    function SetLevel([JRPCParams] ASetLevelParams: TSetLevelRequestParams): TSetLevelResult;
+  end;
+
+
 
 implementation
 
@@ -76,7 +101,7 @@ implementation
 uses
   MCPConnect.MCP.Invoker;
 
-function TMCPToolsApi.Call(const AName: string; AArguments: TJSONObject; Meta: TJSONObject): TCallToolResult;
+function TMCPToolsApi.CallTool(const AName: string; AArguments: TJSONObject; Meta: TJSONObject): TCallToolResult;
 var
   LInvoker: IMCPInvokable;
   LTool: TObject;
@@ -92,11 +117,11 @@ begin
     LTool := MCPConfig.CreateToolInstance(LNamespace);
     try
       LInvoker := TMCPObjectInvoker.Create(LTool);
-      Context.Inject(LInvoker);
-      Context.Inject(LTool);
+      RPCContext.Inject(LInvoker);
+      RPCContext.Inject(LTool);
 
       // Invoke using the tool name without namespace
-      if not LInvoker.Invoke(LToolName, AArguments, Meta, Result) then
+      if not LInvoker.InvokeTool(LToolName, AArguments, Meta, Result) then
         raise EJRPCMethodNotFoundError.CreateFmt('Tool "%s" not found in namespace "%s"', [LToolName, LNamespace]);
     finally
       LTool.Free;
@@ -107,37 +132,24 @@ begin
   end;
 end;
 
-procedure TMCPNotificationsApi.Cancelled([JRPCParams] ACancelledParams: TCancelledNotificationParams);
+procedure TMCPNotificationsApi.Cancelled(ACancelledParams: TCancelledNotificationParams);
 begin
 
 end;
 
 procedure TMCPNotificationsApi.Initialized;
 begin
+
 end;
 
-function TMCPToolsApi.List: TListToolsResult;
+function TMCPToolsApi.ToolsList: TListToolsResult;
 var
-  LToolClasses: TArray<TToolClassInfo>;
-  LClassInfo: TToolClassInfo;
-  LNamespaceSeparator: string;
+  LClassInfo: TMCPClassInfo;
 begin
   Result := TListToolsResult.Create;
   try
-    LToolClasses := MCPConfig.GetToolClasses;
-    LNamespaceSeparator := MCPConfig.GetNamespaceSeparator;
-
-    // Iterate through all registered tool classes
-    for LClassInfo in LToolClasses do
-    begin
-      // Add tools directly to Result with namespace prefix
-      TMCPSchemaGenerator.ListTools(
-        LClassInfo.ToolClass,
-        Result,
-        LClassInfo.Namespace,
-        LNamespaceSeparator
-      );
-    end;
+    for LClassInfo in MCPConfig.Tools.GetClasses do
+      TMCPToolsListGenerator.ListTools(LClassInfo.MCPClass, Result);
   except
     Result.Free;
     raise;
@@ -146,30 +158,76 @@ end;
 
 { TMCPInitializeApi }
 
-function TMCPInitializeApi.Initialize(
-  AInitializeParams: TInitializeParams): TInitializeResult;
+function TMCPInitializeApi.Initialize(AInitializeParams: TInitializeParams): TInitializeResult;
 begin
   Result := TInitializeResult.Create;
   try
     Result.ProtocolVersion := AInitializeParams.ProtocolVersion;
     Result.Capabilities.Tools.ListChanged := False;
+    Result.Capabilities.Resources.ListChanged := False;
+    Result.Capabilities.Resources.Subscribe := False;
 
-    // At the moment we do not support prompts and resources 
+    // At the moment we do not support prompts
     //Result.Capabilities.Prompts.ListChanged := False;
-    //Result.Capabilities.Resources.ListChanged := False;
-    //Result.Capabilities.Resources.Subscribe := False;
 
     Result.ServerInfo.Name := MCPConfig.ServerName;
     Result.ServerInfo.Version := MCPConfig.ServerVersion;
+    Result.ServerInfo.Description := MCPConfig.ServerDescription;
   except
     Result.Free;
     raise;
   end;
 end;
 
+{ TMCPResourcesApi }
+
+function TMCPResourcesApi.ReadResource(const AName: string; AArguments, Meta: TJSONObject): TReadResourceResult;
+begin
+  Result := TReadResourceResult.Create;
+  { TODO -opaolo -c : Finire 08/02/2026 09:39:18 }
+end;
+
+function TMCPResourcesApi.ResourcesList: TListResourcesResult;
+var
+  LClassInfo: TMCPClassInfo;
+begin
+  Result := TListResourcesResult.Create;
+  try
+    for LClassInfo in MCPConfig.Resources.GetClasses do
+      TMCPResourcesListGenerator.ListResources(LClassInfo.MCPClass, Result);
+  except
+    Result.Free;
+    raise;
+  end;
+end;
+
+function TMCPResourcesApi.TemplatesList: TListResourceTemplatesResult;
+var
+  LClassInfo: TMCPClassInfo;
+begin
+  Result := TListResourceTemplatesResult.Create;
+  try
+    for LClassInfo in MCPConfig.Templates.GetClasses do
+      TMCPResourcesListGenerator.ListTemplates(LClassInfo.MCPClass, Result);
+  except
+    Result.Free;
+    raise;
+  end;
+end;
+
+{ TMCPLoggingApi }
+
+function TMCPLoggingApi.SetLevel(ASetLevelParams: TSetLevelRequestParams): TSetLevelResult;
+begin
+  Result := TSetLevelResult.Create;
+  //Result.Level := ASetLevelParams.Level;
+end;
+
 initialization
   TJRPCRegistry.Instance.RegisterClass(TMCPInitializeApi, MCPNeonConfig);
   TJRPCRegistry.Instance.RegisterClass(TMCPToolsApi, MCPNeonConfig);
-  TJRPCRegistry.Instance.RegisterClass(TMCPNotificationsApi, MCPNeonConfig);
+  TJRPCRegistry.Instance.RegisterClass(TMCPResourcesApi, MCPNeonConfig);
 
+  TJRPCRegistry.Instance.RegisterClass(TMCPNotificationsApi, MCPNeonConfig);
+  TJRPCRegistry.Instance.RegisterClass(TMCPLoggingApi, MCPNeonConfig);
 end.
